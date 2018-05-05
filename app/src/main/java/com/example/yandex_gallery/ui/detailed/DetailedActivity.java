c416cf23ce95791179691684dc17f582429638c0
Kirill Petrov <petrov.kirill.job@gmail.com>
2018-05-05 23:01:52 +0300

    Add toolbars, change app colors

--- app/src/main/java/com/example/yandex_gallery/ui/detailed/DetailedActivity.java
+++ app/src/main/java/com/example/yandex_gallery/ui/detailed/DetailedActivity.java
@@ -1,20 +1,33 @@
 package com.example.yandex_gallery.ui.detailed;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.yandex_gallery.R;
 import com.example.yandex_gallery.ui.base.BaseActivity;
 
+import butterknife.BindView;
+
 public class DetailedActivity extends BaseActivity {
+
+    @BindView(R.id.detailedToolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initToolbar();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.contentFrame, new DetailedFragment())
                     .commit();
         }
+    }
+
+    private void initToolbar() {
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(view -> onBackPressed());
+        toolbar.setTitle("Detailed image");
     }
 
     @Override
